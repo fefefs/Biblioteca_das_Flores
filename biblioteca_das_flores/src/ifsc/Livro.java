72822be4d79f663f612ef38e4ae9d4f9475d9b05
Fernanda Cardoso Serafim <fernanda.cs20@aluno.ifsc.edu.br>
2024-04-24 15:06:04 -0300

    Classe Livro cp
    nome do livro| nome do autor | quantidade de paginas | id | disponibilidade

--- biblioteca_das_flores/src/ifsc/Livro.java
+++ biblioteca_das_flores/src/ifsc/Livro.java
@@ -1,5 +1,59 @@
 package ifsc;
 
 public class Livro {
+	
+	//*******//
+	private String nomeLivro;
+	
+	
+	public String getNomeLivro() {
+		return nomeLivro;
+	}
 
+	public void setNomeLivro(String nomeLivro) {
+		this.nomeLivro = nomeLivro;
+	}
+	//*******//
+	private Autor nomeAutor;
+	public Autor getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public void setNomeAutor(Autor nomeAutor) {
+		this.nomeAutor = nomeAutor;
+	}
+
+	//*******//
+	private long id;
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	//*******//
+	private int quantPaginas;
+	public int getQuantPaginas() {
+		return quantPaginas;
+	}
+
+	public void setQuantPaginas(int quantPaginas) {
+		this.quantPaginas = quantPaginas;
+	}
+
+	//*******//
+	private boolean disponivel;
+	public boolean isDisponivel() {
+		return disponivel;
+	}
+
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+	
+	
+
+	
 }
